c750b9f6b0205214eef7bf0ef2456499c989d2a2
dana@4mile.io <dana@4mile.io>
2022-06-08 20:56:52 +0000

    dedupe trips

---
+++ models/nyt_bikeshare/stg/ny_trips_stg.sql
@@ -0,0 +1,50 @@
+with ny_trips as
+
+( SELECT 
+
+md5(starttime||stoptime||bikeid||start_station_id||end_station_id) as hash_trip_id
+,null as original_trip_id
+,'ny' as city
+,tripduration as duration_sec
+,timestamp(starttime) as start_timestamp
+,start_station_name
+,start_station_id
+,timestamp(stoptime) as end_timestamp
+,end_station_name
+,end_station_id
+,bikeid as bike_id
+,null as zip_code
+,usertype as subscriber_type
+,start_station_latitude
+,start_station_longitude
+,end_station_latitude
+,end_station_longitude
+,birth_year as member_birth_year
+,gender as member_gender
+,st_geogpoint(start_station_longitude, start_station_latitude) start_station_geom
+,st_geogpoint(end_station_longitude, end_station_latitude) end_station_geom
+,st_distance(st_geogpoint(start_station_longitude, start_station_latitude), st_geogpoint(end_station_longitude, end_station_latitude)) min_trip_distance_meters
+,if(start_station_id = end_station_id, true, false) is_returned_to_same_station
+
+from {{ source('bikeshare', 'ny_bikeshare_trips') }}
+
+where start_station_id is not null )
+
+, trips_ranked as (
+
+    SELECT
+
+    *
+    ,dense_rank() over (partition by hash_trip_id order by duration_sec desc) rownum
+
+    from ny_trips
+
+)
+
+select
+
+* except (rownum)
+
+from trips_ranked
+
+where rownum = 1 
---
+++ models/sf_bikeshare/sf_trips.sql
@@ -0,0 +1,27 @@
+SELECT 
+
+md5('sf'||trip_id) as hash_trip_id
+,trip_id as original_trip_id
+,'sf' as city
+,duration_sec
+,start_date as start_timestamp
+,start_station_name
+,start_station_id
+,end_date as end_timestamp
+,end_station_name
+,end_station_id
+,bike_number as bike_id
+,zip_code
+,subscriber_type
+,start_station_latitude
+,start_station_longitude
+,end_station_latitude
+,end_station_longitude
+,member_birth_year
+,member_gender
+,start_station_geom
+,end_station_geom
+,st_distance(start_station_geom, end_station_geom) min_trip_distance_meters
+,if(start_station_id = end_station_id, true, false) is_returned_to_same_station
+
+from {{ source('bikeshare', 'sf_bikeshare_trips') }}
